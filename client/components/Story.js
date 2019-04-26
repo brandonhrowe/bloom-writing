801910e8092aa98db9ca74bc720404723680b9a9
Brandon Rowe <brandonhrowe@gmail.com>
2019-04-26 16:24:02 -0400

    CKEditor implemented

--- client/components/Story.js
+++ client/components/Story.js
@@ -4,28 +4,47 @@ import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 // import {setStory} from '../store'
 import socket from '../socket'
+import CKEditor from '@ckeditor/ckeditor5-react'
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
 
 class Prompt extends React.Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {
-      timestamp: "no timestamp",
-      text: ""
+      timestamp: 'no timestamp',
+      text: ''
     }
   }
 
-  componentDidMount(){
-
-  }
+  componentDidMount() {}
 
   render() {
     return (
       <div className="story-container">
-        <textarea
+        {/* <textarea
           className="story"
           rows="10"
           cols="50"
           placeholder="Start your story here..."
+        /> */}
+        <CKEditor
+          className="story"
+          editor={ClassicEditor}
+          data="<p>Write your story here!</p>"
+          onInit={editor => {
+            // You can store the "editor" and use when it is needed.
+            console.log('Editor is ready to use!', editor)
+          }}
+          onChange={(event, editor) => {
+            const data = editor.getData()
+            console.log({event, editor, data})
+          }}
+          onBlur={editor => {
+            console.log('Blur.', editor)
+          }}
+          onFocus={editor => {
+            console.log('Focus.', editor)
+          }}
         />
       </div>
     )
