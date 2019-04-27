0b56d6adbed092d6ce6b7d45b92d178da0b62cf9
Brandon Rowe <brandonhrowe@gmail.com>
2019-04-27 05:54:26 -0400

    working with compromise and sentencer to generate suggestion sentence"

--- client/components/Story.js
+++ client/components/Story.js
@@ -3,32 +3,71 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 // import {setStory} from '../store'
-import socket from '../socket'
+// import socket from '../socket'
 import CKEditor from '@ckeditor/ckeditor5-react'
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
+import io from 'socket.io-client'
+import {EventEmitter} from 'events'
+const socket = io(window.location.origin)
+const events = new EventEmitter()
 
-class Prompt extends React.Component {
+class Story extends React.Component {
   constructor() {
     super()
     this.state = {
-      timestamp: 'no timestamp',
-      text: ''
+      story: ''
     }
+    // this.handleTextChange = this.handleTextChange.bind(this)
   }
 
   componentDidMount() {}
 
+  // writing(text, shouldBroadcast = true){
+  //   this.setState({
+  //     story: text
+  //   })
+  //   shouldBroadcast && events.emit('type', text)
+  // }
+
+  // handleTextChange(event) {
+  //   event.preventDefault()
+  //   let text = event.target.value
+  //   console.log('text to return', text)
+  //   writing(text)
+  // }
+
+  // socket.on('connect', () => {
+  //   console.log('I have made a persistent two-way connection to the server!')
+  //   socket.emit('join-text', 'prompt')
+  // })
+
+  // socket.on('load', story => {
+  //   console.log('loading story', story)
+  //   writing(story, false)
+  // })
+
+  // socket.on('type-from-server', text => {
+  //   console.log('in type-from-server. Text:', text)
+  //   writing(text, false)
+  // })
+
+  // events.on('type', text => {
+  //   console.log('events.on type called. Text:', text)
+  //   socket.emit('type-from-client', 'prompt', text)
+  // })
+
   render() {
     return (
       <div className="story-container">
-        {/* <textarea
-          className="story"
+        <textarea
+          id="story"
           rows="10"
           cols="50"
           placeholder="Start your story here..."
-        /> */}
-        <CKEditor
-          className="story"
+          // onChange={() => this.handleTextChange()}
+        />
+        {/* <CKEditor
+          id="story"
           editor={ClassicEditor}
           data="<p>Write your story here!</p>"
           onInit={editor => {
@@ -45,11 +84,20 @@ class Prompt extends React.Component {
           onFocus={editor => {
             console.log('Focus.', editor)
           }}
-        />
+        /> */}
       </div>
     )
   }
 }
+
+// const setup = () => {
+//   const textField = document.getElementById('story')
+//   document.body.appendChild(textField)
+//   setupTextField()
+// }
+
+
+// document.addEventListener('DOMContentLoaded', setup)
 
 /**
  * CONTAINER
@@ -68,4 +116,4 @@ const mapState = state => {
 //   }
 // }
 
-export default connect(mapState)(Prompt)
+export default connect(mapState)(Story)
--- server/prompts/prompts.js
+++ server/prompts/prompts.js
@@ -1,5 +1,7 @@
 const router = require('express').Router()
 const unirest = require('unirest')
+const Sentencer = require('sentencer')
+const nlp = require('compromise')
 
 module.exports = router
 
@@ -13,6 +15,117 @@ module.exports = router
 //VP---PSA-N = verb, participle, ('running')
 //VI-UUASA-N = verb, indicative ('ran')
 //VI-UUFSA-N = verb, future ('will run')
+
+// const getRandomWord = async pos => {
+//   await unirest
+//     .get(
+//       `https://wordsapiv1.p.rapidapi.com/words/?random=true&partOfSpeech=${pos}&letterPattern=^[A-Za-z]*$`
+//     )
+//     .header('X-RapidAPI-Host', 'wordsapiv1.p.rapidapi.com')
+//     .header('X-RapidAPI-Key', process.env.WORDS_API_KEY)
+//     .end(result => {
+//       return result.body.word
+//     })
+// }
+
+// Sentencer.configure({
+//   verbList: ["wrestle"],
+//   actions: {
+//     verb_past: async () => {
+//       const verb = await self.verbList[0]
+//       console.log(verb)
+//       return nlp(verb)
+//         .verbs()
+//         .toPastTense()
+//         .out()
+//     },
+//     verb_present: async () => {
+//       const verb = self.verbList[0]
+//       return nlp(verb)
+//         .verbs()
+//         .toPresentTense()
+//         .out()
+//     },
+//     verb_future: async () => {
+//       const verb = self.verbList[0]
+//       return nlp(verb)
+//         .verbs()
+//         .toFutureTense()
+//         .out()
+//     },
+//     adverb: async () => {
+//       const adverb = self.adverbList[0]
+//       return adverb
+//     }
+//   }
+// })
+
+// router.get('/', async (req, res, next) => {
+//   try {
+//     // const verb = await getRandomWord('verb')
+//     // console.log(verb)
+//     // const adverb = await getRandomWord('adverb')
+//     const verb = "wrestle"
+//     await Sentencer.configure({
+//       verbList: [verb],
+//       // adverbList: [adverb]
+//     })
+//     const prompt = Sentencer.make(
+//       'Write a story about {{ an_adjective }} {{ noun }} and several {{ nouns }} {{ verb_past }}.'
+//     )
+//     res.send(prompt)
+//   } catch (error) {
+//     next(error)
+//   }
+// })
+
+router.get('/suggestion', async (req, res, next) => {
+  try {
+    let text =
+      'Tomorrow a dog will live with a cat, and they were named Milo and Otis. They did many things together, such as swimming and fishing. They had a pet fish.'
+    const initialNouns = nlp(text)
+      .nouns()
+      console.log(initialNouns)
+    const nouns = initialNouns.filter(noun => nlp(noun).nouns().hasPlural())
+      .data()
+      .map(nounObj => nounObj.main)
+      console.log(nouns)
+      //need to figure out why filtering out only common nouns (by plural) isnt working
+    Sentencer.configure({
+      nounList: [...nouns]
+    })
+    const tense = nlp(text)
+      .verbs()
+      .conjugation()[0]
+    let suggestion
+    //also need better template suggestion sentences with more variety. look more at metaphorpsum
+    if (tense === 'Past') {
+      suggestion = nlp(
+        Sentencer.make('And the {{ noun }} proceeded to do other things...')
+      )
+        .sentences()
+        .toPastTense()
+        .out()
+    } else if (tense === 'Present') {
+      suggestion = nlp(
+        Sentencer.make('And the {{ noun }} proceeded to do other things...')
+      )
+        .sentences()
+        .toPresentTense()
+        .out()
+    } else if (tense === 'Future') {
+      suggestion = nlp(
+        Sentencer.make('And the {{ noun }} proceeded to do other things...')
+      )
+        .sentences()
+        .toFutureTense()
+        .out()
+    }
+    res.send(suggestion)
+  } catch (error) {
+    next(error)
+  }
+})
 
 router.get('/word/random/:pos', async (req, res, next) => {
   try {
