fd591248a6d935d89057c6e98c4b5771c2a7c46f
Brandon Rowe <brandonhrowe@gmail.com>
2019-04-28 11:59:07 -0400

    list working. now need editing function

--- client/components/StoriesList.js
+++ client/components/StoriesList.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {getStoriesThunk} from '../store'
 import {Link} from 'react-router-dom'
+import history from '../history'
 
 /**
  * COMPONENT
@@ -18,18 +19,18 @@ class StoriesList extends Component {
 
   handleClick(event) {
     const {id} = event.target
-    this.props.history.push(`/story/${id}`)
+    history.push(`/story/${id}`)
   }
 
   render() {
-    const {user, stories, history} = this.props
+    const {user, stories} = this.props
 
     return (
       <div className="stories-list-container">
         <h1>{user.username}'s Stories:</h1>
         {stories.map(story => {
           return (
-            <p id={story.id} key={story.id} className="story-list-item">{story.prompt}</p>
+            <p id={story.id} key={story.id} className="story-list-item" onClick={this.handleClick}>{story.prompt}</p>
           )
         })}
       </div>
