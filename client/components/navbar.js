524a4f3394ec1140d077db76f1f65e7e59a45fe9
Brandon Rowe <brandonhrowe@gmail.com>
2019-05-06 07:25:29 -0400

    more css cleanup

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -30,59 +30,54 @@ class Navbar extends Component {
     const {promptLoad} = this.state
     return (
       <div className="nav-bar-container">
-        <nav>
-          <div className="nav-top-container">
-            <h1 onClick={() => history.push('/home')}>
-              BLOOM<br /> WRITING
-            </h1>
+        <nav className="nav-top-container">
             {isLoggedIn ? (
               <div className="dropdown-container">
-                <div className="dropdown-buttons-container">
-                  <div className="dropdown">
-                    <button
-                      type="button"
-                      className="dropbtn"
-                      onClick={() => history.push('/home')}
-                    >
-                      A Room of One's Own
-                    </button>
-                    <div className="dropdown-content">
-                      <div onClick={this.handleNewStory}>Start a New Story</div>
-                      <div onClick={() => history.push('/yourstories')}>
-                        See Your Stories
-                      </div>
+                <h1 onClick={() => history.push('/home')}>
+                  BLOOM<br /> WRITING
+                </h1>
+                <div className="dropdown">
+                  <button
+                    type="button"
+                    className="dropbtn"
+                    onClick={() => history.push('/home')}
+                  >
+                    A Room of One's Own <i className="arrow down"></i>
+                  </button>
+                  <div className="dropdown-content">
+                    <div onClick={this.handleNewStory}>Start a New Story</div>
+                    <div onClick={() => history.push('/yourstories')}>
+                      See Your Stories
                     </div>
                   </div>
-                  <div className="dropdown">
-                    <button type="button" className="dropbtn">
-                      Metamorphoses (Coming Soon)
-                    </button>
-                  </div>
                 </div>
-                <h4 className="promptLoad" style={{opacity: promptLoad}}>
-                  Waiting for Godot. Or a prompt. Whichever comes first...
-                </h4>
+                <div className="dropdown">
+                  <button type="button" className="dropbtn">
+                    Metamorphoses (Coming Soon)
+                  </button>
+                </div>
                 <a href="#" onClick={handleLogout}>
                   Logout
                 </a>
+                <h4 className="promptLoad" style={{opacity: promptLoad}}>
+                  Waiting for Godot. Or a prompt. Whichever comes first...
+                </h4>
               </div>
             ) : (
               <div className="login-signup">
+                <h1 onClick={() => history.push('/home')}>
+                  BLOOM<br /> WRITING
+                </h1>
                 <Link to="/login">Login</Link>
                 <Link to="/signup">Sign Up</Link>
               </div>
             )}
-          </div>
         </nav>
-        <hr />
       </div>
     )
   }
 }
 
-/**
- * CONTAINER
- */
 const mapState = state => {
   return {
     isLoggedIn: !!state.user.id
@@ -101,11 +96,3 @@ const mapDispatch = dispatch => {
 }
 
 export default connect(mapState, mapDispatch)(Navbar)
-
-/**
- * PROP TYPES
- */
-Navbar.propTypes = {
-  handleLogout: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
