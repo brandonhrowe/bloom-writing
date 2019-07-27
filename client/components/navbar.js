32892930fd1a1faafb53bc0bd3589aaa4e84a146
Brandon Rowe <brandonhrowe@gmail.com>
2019-07-27 12:45:36 -0400

    navbar made responsive

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -28,51 +28,53 @@ class Navbar extends Component {
     const {handleLogout, isLoggedIn} = this.props
     const {promptLoad} = this.state
     return (
-      <div className="nav-bar-container">
-        <nav className="nav-top-container">
-            {isLoggedIn ? (
-              <div className="dropdown-container">
-                <h1 onClick={() => history.push('/home')}>
-                  BLOOM<br /> WRITING
-                </h1>
-                <div className="dropdown">
-                  <button
-                    type="button"
-                    className="dropbtn"
-                    onClick={() => history.push('/home')}
-                  >
-                    A Room of One's Own <i className="arrow down"></i>
-                  </button>
-                  <div className="dropdown-content">
-                    <div onClick={this.handleNewStory}>Start a New Story</div>
-                    <div onClick={() => history.push('/yourstories')}>
-                      See Your Stories
-                    </div>
+      // <div className="nav-bar-container">
+      <nav className="nav-top-container">
+        {isLoggedIn ? (
+          <div className="dropdown-container">
+            <div className="dropdown-options">
+              <div className="title" onClick={() => history.push('/home')}>
+                BLOOM<br /> WRITING
+              </div>
+              <div className="dropdown room-of-ones-own">
+                <button
+                  type="button"
+                  className="dropbtn"
+                  onClick={() => history.push('/home')}
+                >
+                  A Room of One's Own <i className="arrow down" />
+                </button>
+                <div className="dropdown-content">
+                  <div onClick={this.handleNewStory}>Start a New Story</div>
+                  <div onClick={() => history.push('/yourstories')}>
+                    See Your Stories
                   </div>
                 </div>
-                <div className="dropdown">
-                  <button type="button" className="dropbtn">
-                    Metamorphoses (Coming Soon)
-                  </button>
-                </div>
-                <a href="#" onClick={handleLogout}>
-                  Logout
-                </a>
-                <h4 className="promptLoad" style={{opacity: promptLoad}}>
-                  Waiting for Godot. Or a prompt. Whichever comes first...
-                </h4>
               </div>
-            ) : (
-              <div className="login-signup">
-                <h1 onClick={() => history.push('/home')}>
-                  BLOOM<br /> WRITING
-                </h1>
-                <Link to="/login">Login</Link>
-                <Link to="/signup">Sign Up</Link>
+              <div className="dropdown metamorphoses">
+                <button type="button" className="dropbtn">
+                  Metamorphoses (Coming Soon)
+                </button>
               </div>
-            )}
-        </nav>
-      </div>
+              <a href="#" onClick={handleLogout}>
+                Logout
+              </a>
+            </div>
+            <h4 className="promptLoad" style={{opacity: promptLoad}}>
+              Waiting for Godot. Or a prompt. Whichever comes first...
+            </h4>
+          </div>
+        ) : (
+          <div className="login-signup">
+            <h1 onClick={() => history.push('/home')}>
+              BLOOM<br /> WRITING
+            </h1>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        )}
+      </nav>
+      // </div>
     )
   }
 }
